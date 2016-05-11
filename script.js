22ef22a46c6014377b3ae31ccbe2e075bad66dcc
Tim <zealnz@gmail.com>
2016-05-11 19:26:06 +1200

    Search params, styling
    Expanded the search parameters to include all the ones that were in the
    Processing bot – though I had an older copy.
    Some styling and minor structural changes. Added support for
    aktiv-grotesk.

--- script.js
+++ script.js
@@ -37,14 +37,53 @@ function tick(){
 function displayTweet(tweet_index){
     $('.debug').html(tweet_index);
     $('.tweet').html(tweet_array[tweet_index].text);
-    $('.author img').attr('src', tweet_array[tweet_index].user.profile_image_url);
+    $('.metadata img').attr('src', tweet_array[tweet_index].user.profile_image_url);
     $('.name').html(tweet_array[tweet_index].user.name);
     $('.screen-name').html('@' + tweet_array[tweet_index].user.screen_name);
-    $('.timestamp').html(tweet_array[tweet_index].created_at);
+    $('.timestamp').html(parseTwitterDate(tweet_array[tweet_index].created_at));
     
     if(tweet_array[tweet_index].entities.media){
         $('main').css({'background-image': 'url('+ tweet_array[tweet_index].entities.media[0].media_url +')'})
     }else{
         $('main').css({'background-image': 'none'})
     }
+    
+    if(tweet_array[tweet_index].retweeted_status){
+        $('.tweet-type').html('retweeted');
+    }else{
+        $('.tweet-type').html('tweeted');
+    }
 }
+
+
+
+
+// thanks Brady: http://stackoverflow.com/users/367154/brady
+function parseTwitterDate(twitter_date) {
+    var system_date = new Date(Date.parse(twitter_date));
+    var now_date = new Date();
+    if (K.ie) {
+        system_date = Date.parse(tdate.replace(/( \+)/, ' UTC$1'))
+    }
+    var diff = Math.floor((now_date - system_date) / 1000);
+    if (diff <= 1) {return "just now";}
+    if (diff < 20) {return diff + " seconds ago";}
+    if (diff < 40) {return "half a minute ago";}
+    if (diff < 60) {return "less than a minute ago";}
+    if (diff <= 90) {return "one minute ago";}
+    if (diff <= 3540) {return Math.round(diff / 60) + " minutes ago";}
+    if (diff <= 5400) {return "1 hour ago";}
+    if (diff <= 86400) {return Math.round(diff / 3600) + " hours ago";}
+    if (diff <= 129600) {return "1 day ago";}
+    if (diff < 604800) {return Math.round(diff / 86400) + " days ago";}
+    if (diff <= 777600) {return "1 week ago";}
+    return "on " + system_date;
+}
+
+// from http://widgets.twimg.com/j/1/widget.js
+var K = function () {
+    var a = navigator.userAgent;
+    return {
+        ie: a.match(/MSIE\s([^;]*)/)
+    }
+}();
